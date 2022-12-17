6e9b90102d9a4e4fc136efa8c62c535d4e6648f7
Esteban Quintana <estebanquintana@MacBook-Air-de-Esteban.local>
2022-12-17 17:11:58 -0600

    Adding tailwind to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  output: {
-    path: path.join(__dirname, "/dist"), // the bundle output path
-    filename: "bundle.js", // the name of the bundle
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "src/index.html", // to import index.html file inside index.js
-    }),
-  ],
-  devServer: {
-    port: 3030, // you can change the port
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/, // .js and .jsx files
-        exclude: /node_modules/, // excluding the node_modules folder
-        use: {
-          loader: "babel-loader",
+    entry: './src/index.jsx',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index_bundle.js',
+    },
+    target: 'web',
+    devServer: {
+        port: '3000',
+        static: {
+            directory: path.join(__dirname, 'public'),
         },
-      },
-      {
-        test: /\.(sa|sc|c)ss$/, // styles files
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
-        options: { limit: false },
-      },
+        open: true,
+        hot: true,
+        liveReload: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html'),
+        }),
     ],
-  },
 };
