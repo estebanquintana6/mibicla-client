939d02e998154ccd59d5eb7f3d0913d72a628d0d
Esteban Quintana <estebanquintana6@gmail.com>
2023-04-15 15:31:57 -0600

    Added no events display

--- components/EventsPage/EventList/EventList.jsx
+++ components/EventsPage/EventList/EventList.jsx
@@ -1,20 +1,36 @@
 import React from "react";
 
+import Image from 'next/image';
+
 import EventCard from "@components/EventsPage/EventCard";
 
 const EventList = ({ eventList, admin }) => {
 
     const gridDisplay = admin ?
         "xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3" :
-        "xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4";
+        "h-full xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4";
+
+    const eventCards = eventList.map((event) =>
+        <EventCard key={event._id}  {...event} />);
 
     return (
-        <section className="bg-white py-6 min-h-screen">
-            <div className={`grid justify-center ${gridDisplay}`}>
-                {eventList.map((event) => 
-                    <EventCard key={event._id}  {...event} />
-                )}
+        <section className="bg-white min-h-[80vh]">
+            {(eventList.length > 0 || admin) ?
+                <div className={`grid justify-center ${gridDisplay} py-6`}>
+                    {eventCards}
+                </div> :
+            <div className="absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-2/4 flex xs:flex-col md:flex-row xs:w-full md:w-auto">
+                <Image
+                    alt="logo"
+                    src={"/no_events.svg"}
+                    width={300}
+                    height={300}
+                />
+                <h1 className="xs:text-xl md:text-3xl font-bold leading-tight tracking-tight text-gray-900 my-auto ml-6 text-center">
+                    No hay eventos por el momento. <br/><span class="text-primary">Estamos trabajando para traer las mejores carreras.</span>
+                </h1>
             </div>
+            }
         </section>
     )
 }
